741a713bd40c11d898069c64b568dab1c56ecc94
Tt <tioo@foxmail.com>
2021-06-01 02:57:20 +0000

    refactor the code

--- main.go
+++ main.go
@@ -1,38 +1,12 @@
 package main
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
-
-	"github.com/speps/go-hashids/v2"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
-
-type jsonResult struct {
-	Msg  string `json:"msg"`
-	Succ bool   `json:"succ"`
-	Url  string `json:"url"`
-}
-
-type shortUrl struct {
-	Id  int64 `gorm:"primarykey"`
-	Url string
-}
-
-type config struct {
-	Id   uint `gorm:"primarykey"`
-	Salt string
-	Min  int
-}
-
-const IDKEY = "__short_url_id_key__"
-const URLKEY = "id:%s"
 
 func main() {
 
@@ -41,47 +15,9 @@ func main() {
 		panic(fmt.Errorf("origin url can not be empty"))
 	}
 
-	info, _ := os.Stat("./data/")
-	if info == nil {
-		os.Mkdir("./data/", os.ModePerm)
-	}
-
-	db, err := gorm.Open(sqlite.Open("./data/url.db"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	db.AutoMigrate(&shortUrl{})
-	db.AutoMigrate(&config{})
-
-	var config config
-	db.Find(&config)
-	if config.Id == 0 {
-
-		salt := os.Getenv("salt")
-		if len(salt) == 0 {
-			file, _ := os.OpenFile("/dev/urandom", os.O_RDONLY, 0)
-			bytes := make([]byte, 16)
-			file.Read(bytes)
-			salt = base64.StdEncoding.EncodeToString(bytes)
-		}
-		config.Salt = salt
-
-		min, err := strconv.Atoi(os.Getenv("minlength"))
-		if err != nil {
-			min = 5
-		}
-		config.Min = min
-
-		db.Save(&config)
-	}
-
-	hd := hashids.NewData()
-	hd.Salt = config.Salt
-	hd.MinLength = config.Min
-	h, _ := hashids.NewWithData(hd)
-
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		// post request
+		// save data and return the short url
 		if r.Method == http.MethodPost {
 			url := r.FormValue("url")
 			if len(url) == 0 {
@@ -92,20 +28,24 @@ func main() {
 				Url: url,
 			}
 			db.Create(&data)
-			shortId, err := h.EncodeInt64([]int64{data.Id})
+			shortId, err := hash.EncodeInt64([]int64{data.Id})
 			if err != nil {
 				result(rw, false, "get short id error, try again", "")
 				return
 			}
 			result(rw, true, "success", fmt.Sprintf("%s/%s", origin, shortId))
 		} else {
+			// other request(get, put and more)
+			// get the short url data, and redirect to the origin url
 			shortId := strings.TrimLeft(r.URL.Path, "/")
 			if len(shortId) > 1 {
-				ids := h.DecodeInt64(shortId)
-				var shortUrl shortUrl
-				db.Find(&shortUrl, ids)
-				if len(shortUrl.Url) > 0 {
-					http.Redirect(rw, r, shortUrl.Url, http.StatusFound)
+				ids, err := hash.DecodeInt64WithError(shortId)
+				if err == nil {
+					var shortUrl shortUrl
+					db.Find(&shortUrl, ids)
+					if shortUrl.Id > 0 {
+						http.Redirect(rw, r, shortUrl.Url, http.StatusFound)
+					}
 				}
 			}
 			rw.WriteHeader(http.StatusNotFound)
